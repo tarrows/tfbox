22c1837c56b606fd9f773e3eae7d382ab6a32eed
c10t <c10t@fa.ke>
2018-10-08 23:58:00 +0900

    create generator

---
+++ work/dcgan.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from keras.layers.convolutional import Conv2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers.core import Reshape
+from keras.layers.convolutiona import UpSampling2D
+from keras.models import Sequential
+
+
+def generator_model():
+    model = Sequential()
+    model.add(Dense(1024, input_shape=(100, ), activation='tanh'))
+    model.add(Dense(128 * 7 * 7))
+    model.add(BatchNormalization)
+    model.add(Activation('tanh'))
+    model.add(Reshape((7, 7, 128), input_shape=(7 * 7 * 128,)))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(64, (5, 5), padding='same', activation='tanh',
+                     data_format='channels_last'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(1, (5, 5), padding='same', activation='tanh',
+                     data_format='channels_last'))
+    return model
